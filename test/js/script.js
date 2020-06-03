dd3cdc745313a4e9f463bbc57d59c7019242e29c
Valentin Hekker <valentin.hekker@edu-kw1c.nl>
2020-06-03 14:23:18 +0200

    jsgf

--- test/js/script.js
+++ test/js/script.js
@@ -1,49 +1,51 @@
-function collision($div1, $div2) {
-	var x1 = $div1.offset().left;
-	var y1 = $div1.offset().top;
-	var h1 = $div1.outerHeight(true);
-	var w1 = $div1.outerWidth(true);
-	var b1 = y1 + h1;
-	var r1 = x1 + w1;
-	var x2 = $div2.offset().left;
-	var y2 = $div2.offset().top;
-	var h2 = $div2.outerHeight(true);
-	var w2 = $div2.outerWidth(true);
-	var b2 = y2 + h2;
-	var r2 = x2 + w2;
-
-	if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-	return true;
-}
-
-var sprite = $(".sprite");
-var ground = $(".ground");
-var spriteTop = $(".sprite").position().top;
-var pause = false
-
-var zwaartekracht = window.setInterval(
-  function()
-  {
-		if (!pause) {
-			if( collision( sprite, ground) == false)
-			{
-				spriteTop += 2;
-				$(".sprite").css('top', spriteTop)
-			}
-		}
-}, 10);
+// var sprite = $(".sprite");
+// var ground = $(".ground");
+// var spriteTop = $(".sprite").position().top;
+// var pause = false
 
 $(document).keydown
 (
   function(e) {
     switch (e.keyCode) {
       case 32:
-      pause = true;
-      $(".sprite").animate({top: "-=50"});
-			pause = false;
+      $(".sprite").animate({top: "-=150"});
+        break;
+      case 39:
+      $(".sprite").css({left: "+=10"});
+        break;
+      case 37:
+      $(".sprite").css({left: "=10"});
         break;
       default:
       return;
     }
   }
 );
+(function() {
+    var gravity = $('.gravity'),
+        obstacle = $('.obstacle');
+    var all = gravity.add(obstacle);
+    setInterval(function() {
+        gravity.each(function() {
+            var e = this,
+                g = $(this),
+                ypos = g.offset().top,
+                xpos = g.offset().left,
+                h = g.height(),
+                w = g.width();
+            var conflicts = false;
+            all.each(function() {
+                if(this === e) return;
+                var a = $(this);
+                if(xpos < a.offset().left + a.width() && xpos + w > a.offset().left) {
+                    if(ypos + h > a.offset().top && ypos + h < a.offset().top + a.height()) {
+                         conflicts = true;
+                    }
+                }
+            });
+            if(!conflicts) {
+                g.css('top', g.offset().top + 10);
+            }
+        });
+    }, 50);
+})();
