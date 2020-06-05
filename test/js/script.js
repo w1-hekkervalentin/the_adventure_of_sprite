e6ecc85c654f4dee30f91c952d5fcb07d292ea73
Valentin Hekker <valentin.hekker@edu-kw1c.nl>
2020-06-05 14:06:07 +0200

    springen en loop animatie

--- test/js/script.js
+++ test/js/script.js
@@ -4,53 +4,53 @@
 // var pause = false
 var lastKey
 
-$(document).keydown
-(
-  function(e) {
-    // switch (e.keyCode) {
-    //   case 32:
-    //   // springen
-    //   $(".sprite").animate({top: "-=150"});
-    //     break;
-    //   case 39:
-    //   // naar rechts
-    //   $(".sprite").css({left: "+=10"});
-    //     break;
-    //   case 37:
-    //   // naar links
-    //   $(".sprite").css({left: "-=10"});
-    //     break;
-    //   default:
-    //   return;
-    // }
-
-
+$(document).keydown(function(e) {
     // naar rechts
     if(e.keyCode == 39) {
+
       $(".sprite").css({left: "+=5", transform: "scaleX(1)"});
       lastKey = e.keyCode;
     }
     // naar links
     if(e.keyCode == 37) {
+
       $(".sprite").css({left: "-=5", transform: "scaleX(-1)"});
       lastKey = e.keyCode;
     }
     // springen
     if(e.keyCode == 32) {
       if (lastKey == 39) {
-        console.log(lastKey);
-        $(".sprite").animate({left: "+=25", top: "-=150", left: "+=50"});
+        $(".sprite").attr("src","../Assets/Characters/The Hero/Jump/Jump.gif")
+        $(".sprite").animate({left: "+=25", top: "-=150", left: "+=50"})
       }
       else if (lastKey == 37) {
-        console.log(lastKey);
-        $(".sprite").animate({left: "-=25", top: "-=150", left: "-=50"});
+        $(".sprite").attr("src","../Assets/Characters/The Hero/Jump/Jump.gif")
+        $(".sprite").animate({left: "-25", top: "-=150", left: "-=50"})
+
       }
       else {
+        $(".sprite").attr("src","../Assets/Characters/The Hero/Jump/Jump.gif")
         $(".sprite").animate({ top: "-=150"});
       }
     }
+    setInterval(function(){
+      if (lastKey == 39) {
+        $(".sprite").attr("src","../Assets/Characters/The Hero/Walk/Walk.gif")
+      }
+      else if (lastKey == 37) {
+        $(".sprite").attr("src","../Assets/Characters/The Hero/Run/Run.gif")
+      }
+    }, 900);
   }
 );
+$(document).keyup(function() {
+  setTimeout(
+  function()
+  {
+    $(".sprite").attr("src","../Assets/Characters/The Hero/Idle/Idle.gif");
+  }, 900);
+  lastKey = 0;
+});
 (function() {
     var gravity = $('.gravity'),
         obstacle = $('.obstacle');
